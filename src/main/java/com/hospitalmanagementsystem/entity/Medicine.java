55e66367b7f97b0bfb0a2b18e43a682a434aee5d
anatolii <batkovich327@gmail.com>
2024-06-22 11:57:16 +0200

    HMS - added Appointment and Medicine entities

---
+++ src/main/java/com/hospitalmanagementsystem/entity/Medicine.java
@@ -0,0 +1,17 @@
+package com.hospitalmanagementsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String drugName;
+    private String stock;
+}
