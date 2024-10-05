384e5d5cd150507f768198f3f2a06d68d8b578ad
anatolii <batkovich327@gmail.com>
2024-10-05 13:52:34 +0200

    HMS-UI - Added UI module

---
+++ hospital-management-system-ui/src/app/patient.ts
@@ -0,0 +1,12 @@
+export class Patient {
+
+    id:number=0;
+    firstName:string="";
+    lastName:string="";
+    age:number=0;
+    bloodGroup:string="";
+    prescription:string="";
+    dose:string="";
+    fees:string="";
+    urgency:string="";
+}
